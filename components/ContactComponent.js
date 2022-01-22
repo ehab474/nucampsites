4f53922e74e28fd84fc2633c646ac8c59f42f9c3
ehab <eehab47@outlook.com>
2022-01-22 11:45:05 -0800

    End of workshop 1

---
+++ components/ContactComponent.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { ScrollView ,Text} from 'react-native';
+import { Card, ListItem, Button, Icon } from 'react-native-elements'
+class Contact extends Component {
+
+    static navigationOptions ={
+        title: 'Contact Us'
+    };
+
+    render() {
+        return (
+            <ScrollView>
+                    <Card title="Contact information" wrapperStyle={{
+                    margin:20
+                    }}>
+                    <Text>1 Nucamp Way</Text>
+                    <Text>Seattle, WA 98001
+                    </Text>
+                    <Text>                      
+                    U.S.A.
+                    </Text>
+                    <Text>
+                    Phone: 1-206-555-1234
+                    </Text>
+                    <Text style={{
+                        marginBottom:10
+                    }}>
+                    Email: campsites@nucamp.co
+                    </Text>
+                    </Card>
+            </ScrollView>
+        );
+    };
+}
+
+export default Contact;
